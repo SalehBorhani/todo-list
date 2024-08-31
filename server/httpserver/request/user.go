b2c5a67582901dbf6acc560b1934e262cad19724
borhanisaleh6 <borhanisaleh6@gmail.com>
2024-09-01 00:35:45 +0330

    feat: add login user api with basic-auth

--- server/httpserver/request/user.go
+++ server/httpserver/request/user.go
@@ -1,37 +1,12 @@
 package request
 
-import (
-	"encoding/json"
-	"github.com/google/uuid"
-	"github.com/salehborhani/todo-list/entity"
-	"github.com/salehborhani/todo-list/repository/filestorage"
-	"net/http"
-)
-
-type Register struct {
+type UserRegister struct {
+	Email    string `json:"email"`
 	UserName string `json:"user_name"`
 	Password string `json:"password"`
 }
 
-func RegisterUser(w http.ResponseWriter, r *http.Request) {
-
-	// Create or Open fileStore and load to memoryStore `users` variable
-	fs := filestorage.New("users.json")
-	users := fs.Load()
-
-	var reg Register
-	if err := json.NewDecoder(r.Body).Decode(&reg); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	user := entity.User{
-		ID:       uuid.New(),
-		UserName: reg.UserName,
-		Password: reg.Password,
-	}
-
-	users = append(users, user)
-	fs.Save(user)
-
+type UserLogin struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
 }
--- server/httpserver/root.go
+++ server/httpserver/root.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/salehborhani/todo-list/server/httpserver/request"
-	"log"
+	"github.com/salehborhani/todo-list/server/httpserver/handler"
 	"net/http"
 )
 
 func main() {
 
-	// TODO - register user with name, email and password
-	http.HandleFunc("/api/register/", request.RegisterUser)
+	http.HandleFunc("/api/register/", handler.RegisterUser)
+
+	// TODO - login user with the creds
+
+	http.HandleFunc("/api/login/", handler.LoginUser)
+
 	server := &http.Server{
 		Addr: ":8080",
 	}
 	fmt.Println("Starting server on", server.Addr)
-
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatalln(err)
+
 	}
-
-	// TODO - login user with the creds
-
 }
